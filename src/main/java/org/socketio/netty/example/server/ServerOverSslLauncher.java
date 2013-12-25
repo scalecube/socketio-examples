b6dbde25b3afef0d87e7caeb49bc67598dc62a12
Anton Kharenko <a.kharenko@gmail.com>
2013-12-26 00:15:39 +0200

    Replace date.format.js with moment.js, simplify server launchers and change versions in pom

--- src/main/java/org/socketio/netty/example/server/ServerOverSslLauncher.java
+++ src/main/java/org/socketio/netty/example/server/ServerOverSslLauncher.java
@@ -1,21 +1,16 @@
 package org.socketio.netty.example.server;
 
-import javax.net.ssl.SSLContext;
-
 import org.socketio.netty.SocketIOServer;
 import org.socketio.netty.example.ssl.SecureSslContextFactory;
 
 public class ServerOverSslLauncher {
 	
 	private static final int SOCKETIO_PORT = 4815;
-	private static final String SOCKETIO_TRANSPORTS = "websocket,xhr-polling";
 	
 	public static void main(String[] args) {
 		SocketIOServer socketioOverSSLServer = new SocketIOServer();
 		socketioOverSSLServer.setPort(SOCKETIO_PORT);
-		socketioOverSSLServer.setTransports(SOCKETIO_TRANSPORTS);
-		SSLContext sslContext = SecureSslContextFactory.getServerContext();
-		socketioOverSSLServer.setSslContext(sslContext);
+		socketioOverSSLServer.setSslContext(SecureSslContextFactory.getServerContext());
 		socketioOverSSLServer.setListener(new EchoSocketIOListener());
 		socketioOverSSLServer.start();
 	}
