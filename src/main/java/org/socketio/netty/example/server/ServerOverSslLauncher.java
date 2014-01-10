40e2b1d142334abb99b20403a3865412aa86d176
Anton Kharenko <a.kharenko@gmail.com>
2014-01-10 12:25:10 +0200

    Add to examples settings for timeouts

--- src/main/java/org/socketio/netty/example/server/ServerOverSslLauncher.java
+++ src/main/java/org/socketio/netty/example/server/ServerOverSslLauncher.java
@@ -6,11 +6,17 @@ import org.socketio.netty.example.ssl.SecureSslContextFactory;
 public class ServerOverSslLauncher {
 	
 	private static final int SOCKETIO_PORT = 4815;
+	private static final int HEARTBEAT_INTERVAL = 60;
+	private static final int HEARTBEAT_TIMEOUT = 90;
+	private static final int CLOSE_TIMEOUT = 75;
 	private static final String SOCKETIO_TRANSPORTS = "websocket,flashsocket,xhr-polling,jsonp-polling";
 	
 	public static void main(String[] args) {
 		SocketIOServer socketioOverSSLServer = new SocketIOServer();
 		socketioOverSSLServer.setPort(SOCKETIO_PORT);
+		socketioOverSSLServer.setHeartbeatInterval(HEARTBEAT_INTERVAL);
+		socketioOverSSLServer.setHeartbeatTimeout(HEARTBEAT_TIMEOUT);
+		socketioOverSSLServer.setCloseTimeout(CLOSE_TIMEOUT);
 		socketioOverSSLServer.setTransports(SOCKETIO_TRANSPORTS);
 		socketioOverSSLServer.setSslContext(SecureSslContextFactory.getServerContext());
 		socketioOverSSLServer.setListener(new EchoSocketIOListener());
